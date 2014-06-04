e444354d5f08b50a5441c2611b5dd8b549a20d08
Vijay Thurimella <vthurimella@cmu.edu>
2014-06-03 19:08:59 -0600

    rewrote logic in lockpic and moved daylog functionality into lockpic

--- lockpic.sh
+++ lockpic.sh
@@ -1,60 +1,44 @@
 #!/bin/bash
 
-lockfldr="/home/vijay/Pictures/Login/"
-droplockfldr="/home/vijay/Dropbox/Photos/Login/"
-
-logfldr="/home/vijay/Pictures/DayLog"
-dropfldr="/home/vijay/Dropbox/Photos/DayLog"
-now=$(date +%s)
+# picfldr="/home/vijay/Pictures/Login/"
+# dropboxpicfldr="/home/vijay/Dropbox/Photos/Login/"
 
 get_last(){
-	ls $logfldr | sort -n | tail -1 | cut -d. -f1
+	ls -1 $picfldr/ | grep .txt | sort -n | tail -1 | cut -d. -f1
 }
 
 calc(){
 	python -c "print $1"
 }
 
-take_log_pic(){
-	cd $logfldr
-	zenity --entry --text "What did you do today?" --title "DayLog" >> $now.txt
-	streamer -f jpeg -o $now.jpeg -s 1280x720 >> /dev/null 2>&1
-	cd - >> /dev/null 2>&1
+cp_dropbox() {
+	if [[ ! -z $droplockfldr ]]; then
+		cp $1 $droplockfldr
+	fi
 }
 
-cp_dropbox(){
-	cd $logfldr
-	cp $now.txt $dropfldr
-	cp $now.jpeg $dropfldr
-	cd - >> /dev/null 2>&1
-}
 take_pic(){
+	cd $picfldr
+	now=$(date +%s)
 
-	#Check if one day has occur to associate the daylog
-	#with some text 
+	# Take a photo
+	streamer -f jpeg -o $now.jpeg -s 1280x720 >> /dev/null 2>&1
+	cp_dropbox $now.jpeg
+
+
+	#Check whether to add caption
 	prev=$(get_last)
 	if [[ -z $prev ]]; then 
-		take_log_pic
-		if [[ ! -z $dropfldr ]]; then
-			cp_dropbox
-		fi
-
-	else 
+		zenity --entry --text "What did you do today?" --title "DayLog" >> $now.txt
+		cp_dropbox $now.txt
+	else
 		sub=$(calc "$now - $prev")
 		if [[ $sub -ge 86400 ]]; then 
-			take_log_pic
-			if [[ ! -z $dropfldr ]]; then
-				cp_dropbox
-			fi
+			zenity --entry --text "What did you do today?" --title "DayLog" >> $now.txt
+			cp_dropbox $now.txt
 		fi
 	fi
 
-	#Either way take a photo and put in the lock folder
-	cd $lockfldr
-	streamer -f jpeg -o $now.jpeg -s 1280x720 >> /dev/null 2>&1
-	if [[ ! -z $droplockfldr ]]; then
-		cp $now.jpeg $droplockfldr
-	fi
 	cd - >> /dev/null 2>&1
 }
 
