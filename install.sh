4f01fcbe5be3b1ad84d7a3bfc8e55770294b03e0
Vijay Thurimella <vthurimella@cmu.edu>
2014-06-13 00:21:54 -0600

    Simpler way for program to run at login

--- install.sh
+++ install.sh
@@ -39,22 +39,6 @@ fi
 sed -i "3i dropboxfldr="$dropboxfldr $HOME/bin/lockpic
 sed -i "3i picfldr="$fldr $HOME/bin/lockpic
 
-#Add to startup 
-start_loc="$HOME/.config/autostart/lockpic.desktop"
-
-if [[ -f $start_loc ]]; then
-	rm $start_loc
-fi
-
-echo "[Desktop Entry]" >> $start_loc
-echo "Type=Application" >> $start_loc
-echo "Exec=$HOME/bin/lockpic &" >> $start_loc
-echo "Hidden=false" >> $start_loc
-echo "NoDisplay=false" >> $start_loc
-echo "X-GNOME-Autostart-enabled=true" >> $start_loc
-echo "Name[en_US]=Lockpic" >> $start_loc
-echo "Name=Lockpic" >> $start_loc
-echo "Comment[en_US]=Lockpic that takes a photo with the webcam everytime a login or unlock of the computer occurs" >> $start_loc
-echo "Comment=Lockpic that takes a photo with the webcam everytime a login or unlock of the computer occurs" >> $start_loc
+echo "lockpic &" >> .profile
 
 cd - >> /dev/null 2>&1
